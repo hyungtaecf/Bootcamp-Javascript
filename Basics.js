2e3142b99d0b0dd3ffc89c218c30051887ac87e1
Hyung Tae C. Figur <hyu_03@hotmail.com>
2019-09-07 10:09:45 +0900

    First commit
    First commit

---
+++ Basics.js
@@ -0,0 +1,47 @@
+alert("Hello World!") //pop an alert up
+// anything here is a comment
+// BASIC DATA TYPES
+// NUMBERS
+10
+10.2
+-21
+// STRINGS
+"Hello World"
+"10"
+// BOOLEANS
+true
+false
+//UNDEFINED AND NULL
+undefined //something was created but never was actually defined
+null //actually it doesn't have anything in the variable
+
+
+//CLEAR CONSOLE clear()
+
+//Operacoes
+2 ** 4 //-- 2 elevado a 4 --   = 16
+15 % 14 // -- modulo --     = 1
+
+"Django is cool"
+"Django" + " is super cool" // = Django is super cool
+"django".length // = 6
+"hello \n start a new line"
+"hello \"quotes\" " // = hello "quotes"
+"hello[4]" // = "o"
+
+//VARIABLES
+//var varName = value; // Agora ponto-virgula eh opcional
+
+var bankAccount = 100;
+bankAccount // = 100
+var deposit = 50;
+var total = bankAccount + deposit; // = 150
+
+var greeting = "Welcome back: ";
+var name = "Jose";
+alert(greeting+name);
+
+console.log("Hey I'm in the console"); //Print on the console
+prompt("Enter something") // Pops up and you can enter something
+var age = prompt("Enter your age: ")
+age // comes your input for age
---
+++ myscript.js
@@ -0,0 +1,4 @@
+alert("Welcome to your Bank!")
+var deposit = prompt("How much you want to deposit today: ")
+alert("You deposited: "+deposit)
+console.log("You are a cool person!")

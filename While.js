4b23c3548c3d6b0d3fc8734419b04a26fdabe77d
Hyung Tae C. Figur <hyu_03@hotmail.com>
2019-09-09 17:33:05 +0900

    First Commit
    First Commit

---
+++ While.js
@@ -0,0 +1,21 @@
+var x = 0;
+
+// while (x<5){
+//     console.log("x is currently: "+x);
+
+//     if (x===3){
+//         console.log("X IS EQUAL TO THREE!")
+//         break;
+//     }
+//     console.log("x is still less than 5, adding 1 to x");
+//     x = x+1;
+// }
+
+// EVEN NUMBERS FROM 1 TO 10
+x = 1;
+while(x<=10){
+    if(x%2===0){
+        console.log(x);
+    }
+    x++;
+}
